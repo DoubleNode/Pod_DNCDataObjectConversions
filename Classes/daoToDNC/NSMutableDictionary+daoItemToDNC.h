8e5066c4127661ed5f30a0bebff04a410f6927e3
Darren Ehlers <me@darrenehlers.com>
2019-08-29 15:02:17 -0500

    Open Sourced

--- Classes/daoToDNC/NSMutableDictionary+daoItemToDNC.h
+++ Classes/daoToDNC/NSMutableDictionary+daoItemToDNC.h
@@ -1,9 +1,11 @@
 //
 //  NSMutableDictionary+daoItemToDNC.h
-//  DoubleNode Core
+//  DoubleNode Dsta Object Conversions
 //
 //  Created by Darren Ehlers on 2016/10/16.
-//  Copyright © 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
+//  Copyright © 2016 Darren Ehlers and DoubleNode, LLC.
+//
+//  DNCDataObjectConversions is released under the MIT license. See LICENSE for details.
 //
 
 @import UIKit;
